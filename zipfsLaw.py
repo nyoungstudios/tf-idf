cebac4cee1b477617ba3af3a65562726ddc05278
nyoungstudios <nathanielyoung95070@gmail.com>
2018-06-17 22:58:39 -0700

    creates updates

---
+++ zipfsLaw.py
@@ -0,0 +1,24 @@
+#this program proves zipf's law
+
+
+
+def readFile(fileName):
+    #this function reads the database file into a map for the idf part
+    f = open(fileName, 'r')
+    s = f.read()
+    lineList = s.split("\n")  #list of all the lines from the txt file
+
+    idfMap = {}
+
+    for i in range(len(lineList)-1):
+        stringLine = lineList[i]  #stores a string of one line from the file
+        wordValueList = stringLine.split(":")  #stores a list with the 1st element as the word, and the 2nd element as the idf value
+        idfMap[wordValueList[0]] = wordValueList[1]
+
+
+    f.close()
+    return idfMap
+
+databaseIDFMap = readFile("database.txt")
+
+print(databaseIDFMap["the"])
